dcd1a9d6523196644ebdc7572575cc8c5da6187b
Eduardo Martos <emartos@natiboo.es>
2025-12-27 23:01:06 +0100

    Introduce labdoo-ui.js with basic Drupal behavior structure

---
+++ web/themes/custom/labdoo/js/labdoo-ui.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-bitwise, no-nested-ternary, no-mutable-exports, comma-dangle, strict */
+
+'use strict';
+
+(($, Drupal, drupalSettings) => {
+    Drupal.behaviors.labdoo_ui = {
+        attach: function attach() {
+        }
+    }
+})(jQuery, Drupal, drupalSettings);
