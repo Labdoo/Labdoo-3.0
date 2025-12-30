0978483d0ae60b30b2bce16e09d53c407c57d348
Eduardo Martos <emartos@natiboo.es>
2025-12-30 23:56:03 +0100

    Add language-aware root page redirection to MiniWiki and preprocess menu adjustments in Labdoo Wiki

--- web/modules/custom/mini_wiki/src/Controller/MiniWikiController.php
+++ web/modules/custom/mini_wiki/src/Controller/MiniWikiController.php
@@ -4,6 +4,7 @@ namespace Drupal\mini_wiki\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\mini_wiki\Entity\MiniWikiPage;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -23,8 +24,12 @@ class MiniWikiController extends ControllerBase {
    * Constructs a new instance of the class.
    *
    * @param ConfigFactoryInterface $config_factory The configuration factory interface
+   * @param LanguageManagerInterface $language_manager The language manager interface
    */
-  public function __construct(protected ConfigFactoryInterface $config_factory) {
+  public function __construct(
+    protected ConfigFactoryInterface $config_factory,
+    protected LanguageManagerInterface $language_manager
+  ) {
   }
 
   /**
@@ -32,7 +37,8 @@ class MiniWikiController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('config.factory')
+      $container->get('config.factory'),
+      $container->get('language_manager')
     );
   }
 
@@ -49,14 +55,35 @@ class MiniWikiController extends ControllerBase {
   public function redirectToRoot(): RedirectResponse {
     $config = $this->config_factory->get('mini_wiki_page.settings');
     $entityId = $config->get('root_page');
-    if (empty($entityId)) {
-      throw new NotFoundHttpException('The wiki root page has not been set');
+    $wikiRootPage = NULL;
+    $currentLanguage = $this->language_manager->getCurrentLanguage()->getId();
+
+    if (!empty($entityId)) {
+      // Load the wiki root page.
+      $wikiRootPage = MiniWikiPage::load($entityId);
+      if ($wikiRootPage && $wikiRootPage->hasTranslation($currentLanguage)) {
+        $wikiRootPage = $wikiRootPage->getTranslation($currentLanguage);
+      }
     }
 
-    // Load the wiki root page.
-    $wikiRootPage = MiniWikiPage::load($entityId);
     if (empty($wikiRootPage)) {
-      throw new NotFoundHttpException('The wiki root page set is not valid');
+      $storage = $this->entityTypeManager()->getStorage('mini_wiki_page');
+      $query = $storage->getQuery()
+        ->accessCheck(TRUE)
+        ->condition('parent', NULL, 'IS NULL')
+        ->condition('langcode', $currentLanguage)
+        ->sort('id', 'ASC')
+        ->range(0, 1);
+
+      $ids = $query->execute();
+
+      if (!empty($ids)) {
+        $wikiRootPage = $storage->load(reset($ids));
+      }
+    }
+
+    if (empty($wikiRootPage)) {
+      throw new NotFoundHttpException('The wiki root page has not been set and no default could be found');
     }
 
     $url = $wikiRootPage->toUrl()->toString();
